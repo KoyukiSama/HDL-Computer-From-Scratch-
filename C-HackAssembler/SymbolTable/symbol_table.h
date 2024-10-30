b31650f73fc599255c48d68dc7d44cf8d0a59346
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-10-30 23:48:28 +0100

    add basic header file

--- C-HackAssembler/SymbolTable/symbol_table.h
+++ C-HackAssembler/SymbolTable/symbol_table.h
@@ -1,3 +1,32 @@
 #pragma once
 
-// declarations
+#include <stdlib.h>
+
+#define LOAD_FACTOR 0.75
+#define GROWTH_FACTOR 2
+
+// symbol-table data structure
+
+typedef struct bucket {
+    char* symbol;
+    short value;
+    struct bucket* next;
+} bucket_t;
+
+typedef struct symboltable {
+    bucket_t** bucketlist;
+    size_t size;
+    size_t capacity;
+} symboltable_t;
+
+// symbol-table functions
+
+char            symboltable_init (symboltable_t *SymbolTable, size_t initial_capacity);
+char            symboltable_destroy (symboltable_t *SymbolTable);
+
+unsigned short  symboltable_hashcode (symboltable_t *SymbolTable, char* symbol);
+
+void*           symboltable_set (symboltable_t *SymbolTable, char* symbol, unsigned short value);
+void*           symboltable_get (symboltable_t *SymbolTable, char* symbol);
+
+// malloc handling
