f0bed327af701460c2ccee038a84e0d9666dd77f
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-10-31 01:41:23 +0100

    add Malloc function

--- C-HackAssembler/SymbolTable/symbol_table.c
+++ C-HackAssembler/SymbolTable/symbol_table.c
@@ -1,9 +1,15 @@
 #include "symbol_table.h"
 
 char symboltable_init (symboltable_t *SymbolTable, size_t initial_capacity) {
+    // check if exists
     if (SymbolTable != NULL) {
         return 2;
     }
+
+    // create symboltable
+    SymbolTable->capacity = initial_capacity;
+    SymbolTable->size = 0;
+    bucket_t** BucketList = Malloc(sizeof(**BucketList) * initial_capacity);
 }
 
 char symboltable_destroy (symboltable_t *SymbolTable);
@@ -13,3 +19,15 @@ unsigned short  symboltable_hashcode (symboltable_t *SymbolTable, char* symbol);
 void* symboltable_set (symboltable_t *SymbolTable, char* symbol, unsigned short value);
 
 void* symboltable_get (symboltable_t *SymbolTable, char* symbol);
+
+
+// mallocs
+
+void* Malloc(size_t size_t) {
+    void *ptr = malloc(size_t);
+    if ( ptr == NULL ) {
+        perror("Malloc error");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
