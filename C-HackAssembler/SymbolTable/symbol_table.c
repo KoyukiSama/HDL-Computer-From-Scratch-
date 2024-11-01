1b3924b2e114c01f40be40ba888ba98c172a6c05
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-11-01 03:40:39 +0100

    add symboltable_ensure_capacity and symboltable_get

--- C-HackAssembler/SymbolTable/symbol_table.c
+++ C-HackAssembler/SymbolTable/symbol_table.c
@@ -8,7 +8,7 @@ symboltable_t* symboltable_init(size_t initial_capacity) {
     SymbolTable->size     = 0;
 
     // Bucket list
-    bucket_t** BucketList = SymbolTable->BucketList = Calloc(initial_capacity, sizeof(**BucketList));
+    bucket_t** BucketList = SymbolTable->BucketList = Calloc(initial_capacity, sizeof(*BucketList));
     if (BucketList == NULL) { // free symboltable if error
         free(SymbolTable);
         exit(EXIT_FAILURE);
@@ -59,7 +59,8 @@ char symboltable_set(symboltable_t *SymbolTable, const char* symbol, unsigned sh
         currBucket = currBucket->next;
     }
 
-    // ensure capacity
+    // change capacity if needed
+    symboltable_ensure_capacity( SymbolTable );
 
     // key not in symbol table, alloc new 
     bucket_t *bucket = CallocExit(1, sizeof(*bucket));
@@ -74,7 +75,18 @@ char symboltable_set(symboltable_t *SymbolTable, const char* symbol, unsigned sh
     return 0;
 }
 
-void* symboltable_get(symboltable_t *SymbolTable, const char* symbol);
+void* symboltable_get(symboltable_t *SymbolTable, const char* symbol) {
+    bucket_t *currBucket = SymbolTable->BucketList[symboltable_hashcode(SymbolTable, symbol)];
+
+    while (currBucket) {
+        if (strcmp(currBucket->symbol, symbol) == 0) {
+            return currBucket->value;
+        }
+        currBucket = currBucket->next;
+    }
+    
+    return NULL;
+}
 
 
 
@@ -90,8 +102,27 @@ unsigned short  symboltable_hashcode(symboltable_t *SymbolTable, const char* sym
     return (unsigned short)(hash % SymbolTable->capacity);
 }
 
-void symboltable_ensure_capacity() {
+void symboltable_ensure_capacity(symboltable_t *SymbolTable) {
+    if ( SymbolTable->size > (SymbolTable->capacity * LOAD_FACTOR) ) {
 
+        // set new capacity
+        size_t capacity_old = SymbolTable->capacity;
+        size_t capacity_new = SymbolTable->capacity = SymbolTable->capacity * GROWTH_FACTOR;
+        
+        // re memory
+        bucket_t** new_list = realloc(SymbolTable->BucketList, capacity_new * sizeof(*new_list));
+        if (new_list == NULL) {
+            perror("Realloc error");
+            exit(EXIT_FAILURE);
+        }
+        for (size_t i = capacity_old; i < capacity_new; i++) { // fake calloc
+            new_list[i] = NULL;
+        }
+
+        // set 
+        SymbolTable->BucketList = new_list;
+    }
+    return;
 }
 
 // malloc wrapper
