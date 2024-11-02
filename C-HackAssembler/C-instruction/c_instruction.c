7013c3a1a82395f0964d4cbd330abbb3b3b876f4
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-11-02 14:43:27 +0100

    fix c_instruction.c, create_C_instruction_table(), changed binary digits to decimal notation

--- C-HackAssembler/C-instruction/c_instruction.c
+++ C-HackAssembler/C-instruction/c_instruction.c
@@ -1,59 +1,58 @@
 #include "c_instruction.h"
-#include "../SymbolTable/symbol_table.h"
 
 symboltable_t* create_C_instruction_table() {
     // initialize BinaryTable
     symboltable_t* BinaryTable = symboltable_init(36);
 
     // set comp instructions
-    symboltable_set(BinaryTable, "0", 0b0101010); // 0
-    symboltable_set(BinaryTable, "1", 0b0111111); // 1
-    symboltable_set(BinaryTable, "-1", 0b0111010); // -1
-    symboltable_set(BinaryTable, "D", 0b0001100); // D
-    symboltable_set(BinaryTable, "A", 0b0110000);
-    symboltable_set(BinaryTable, "M", 0b1110000);
-    symboltable_set(BinaryTable, "!D", 0b0001101); // !D
-    symboltable_set(BinaryTable, "!A", 0b0110001);
-    symboltable_set(BinaryTable, "!M", 0b1110001);
-    symboltable_set(BinaryTable, "-D", 0b0001111); // -D
-    symboltable_set(BinaryTable, "-A", 0b0110011);
-    symboltable_set(BinaryTable, "-M", 0b1110011);
-    symboltable_set(BinaryTable, "D+1", 0b0011111); // D+1
-    symboltable_set(BinaryTable, "A+1", 0b0110111);
-    symboltable_set(BinaryTable, "M+1", 0b1110111);
-    symboltable_set(BinaryTable, "D-1", 0b0001110); // D-1
-    symboltable_set(BinaryTable, "A-1", 0b0110010);
-    symboltable_set(BinaryTable, "M-1", 0b1110010);
-    symboltable_set(BinaryTable, "D+A", 0b0000010); // D+A
-    symboltable_set(BinaryTable, "D+M", 0b1000010);
-    symboltable_set(BinaryTable, "D-A", 0b0010011); // D-A
-    symboltable_set(BinaryTable, "D-M", 0b1010011);
-    symboltable_set(BinaryTable, "A-D", 0b0000111); // A-D
-    symboltable_set(BinaryTable, "M-D", 0b1000111);
-    symboltable_set(BinaryTable, "D&A", 0b0000000); // D&A
-    symboltable_set(BinaryTable, "D&M", 0b1000000);
-    symboltable_set(BinaryTable, "D|A", 0b0010101); // D|A
-    symboltable_set(BinaryTable, "D|M", 0b1010101);
+    symboltable_set(BinaryTable, "0", 21); // 0
+    symboltable_set(BinaryTable, "1", 63); // 1
+    symboltable_set(BinaryTable, "-1", 58); // -1
+    symboltable_set(BinaryTable, "D", 12); // D
+    symboltable_set(BinaryTable, "A", 48);
+    symboltable_set(BinaryTable, "M", 112);
+    symboltable_set(BinaryTable, "!D", 13); // !D
+    symboltable_set(BinaryTable, "!A", 49);
+    symboltable_set(BinaryTable, "!M", 113);
+    symboltable_set(BinaryTable, "-D", 15); // -D
+    symboltable_set(BinaryTable, "-A", 51);
+    symboltable_set(BinaryTable, "-M", 115);
+    symboltable_set(BinaryTable, "D+1", 31); // D+1
+    symboltable_set(BinaryTable, "A+1", 55);
+    symboltable_set(BinaryTable, "M+1", 119);
+    symboltable_set(BinaryTable, "D-1", 14); // D-1
+    symboltable_set(BinaryTable, "A-1", 50);
+    symboltable_set(BinaryTable, "M-1", 114);
+    symboltable_set(BinaryTable, "D+A", 2); // D+A
+    symboltable_set(BinaryTable, "D+M", 66);
+    symboltable_set(BinaryTable, "D-A", 19); // D-A
+    symboltable_set(BinaryTable, "D-M", 83);
+    symboltable_set(BinaryTable, "A-D", 7); // A-D
+    symboltable_set(BinaryTable, "M-D", 71);
+    symboltable_set(BinaryTable, "D&A", 0); // D&A
+    symboltable_set(BinaryTable, "D&M", 64);
+    symboltable_set(BinaryTable, "D|A", 21); // D|A
+    symboltable_set(BinaryTable, "D|M", 85);
 
     // set dest instructions
-    symboltable_set(BinaryTable, "null", "0b000"); // null
-    symboltable_set(BinaryTable, "M", "0b001"); // M
-    symboltable_set(BinaryTable, "D", "0b010"); // D
-    symboltable_set(BinaryTable, "MD", "0b011"); // MD
-    symboltable_set(BinaryTable, "A", "0b100"); // A
-    symboltable_set(BinaryTable, "AM", "0b101"); // AM
-    symboltable_set(BinaryTable, "AD", "0b110"); // AD
-    symboltable_set(BinaryTable, "AMD", "0b111"); // AMD
+    symboltable_set(BinaryTable, "null", "0"); // null
+    symboltable_set(BinaryTable, "M", "1"); // M
+    symboltable_set(BinaryTable, "D", "2"); // D
+    symboltable_set(BinaryTable, "MD", "3"); // MD
+    symboltable_set(BinaryTable, "A", "4"); // A
+    symboltable_set(BinaryTable, "AM", "5"); // AM
+    symboltable_set(BinaryTable, "AD", "6"); // AD
+    symboltable_set(BinaryTable, "AMD", "7"); // AMD
 
     // set jump instructions
-    symboltable_set(BinaryTable, "null", "0b000"); // null
-    symboltable_set(BinaryTable, "JGT", "0b001"); // JGT
-    symboltable_set(BinaryTable, "JEQ", "0b010"); // JEQ
-    symboltable_set(BinaryTable, "JGE", "0b011"); // JGE
-    symboltable_set(BinaryTable, "JLT", "0b100"); // JLT
-    symboltable_set(BinaryTable, "JNE", "0b101"); // JNE
-    symboltable_set(BinaryTable, "JLE", "0b110"); // JLE
-    symboltable_set(BinaryTable, "JMP", "0b111"); //JMP
+    symboltable_set(BinaryTable, "null", "0"); // null
+    symboltable_set(BinaryTable, "JGT", "1"); // JGT
+    symboltable_set(BinaryTable, "JEQ", "2"); // JEQ
+    symboltable_set(BinaryTable, "JGE", "3"); // JGE
+    symboltable_set(BinaryTable, "JLT", "4"); // JLT
+    symboltable_set(BinaryTable, "JNE", "5"); // JNE
+    symboltable_set(BinaryTable, "JLE", "6"); // JLE
+    symboltable_set(BinaryTable, "JMP", "7"); // JMP
 }
 
 // helper function
--- C-HackAssembler/C-instruction/c_instruction.h
+++ C-HackAssembler/C-instruction/c_instruction.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include "../SymbolTable/symbol_table.h"
+
 // C-instruction shift MACRO
 #define SHIFTBITS_OPCODE_111(bin_c_instruction) ((c_instruction) | (new_bits))
 #define SHIFTBITS_COMPUTATION(bin_c_instruction, new_bits) ((c_instruction << 7) | (new_bits))
 #define SHIFTBITS_DEST(bin_c_instruction, new_bits) ((c_instruction << 3) | (new_bits))
 #define SHIFTBITS_JUMP(bin_c_instruction, new_bits) ((c_instruction << 3) | (new_bits))
+
+
+symboltable_t* create_C_instruction_table();
+
+void split_C_instruction(char* c_instruction, char* dest, char* comp, char* jump);
+
+unsigned short translate_C_instruction_bin(symboltable_t* BinaryTable, char* c_instruction);
