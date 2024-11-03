91a8e46ce5f0efcb1d88bff7131480e6f0cbed6c
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-11-03 06:28:12 +0100

    fix c_instruction.c, split_C_instruction() in semiColonPos condition, did not assign '\0' to z instead it was at i

--- C-HackAssembler/C-instruction/c_instruction.c
+++ C-HackAssembler/C-instruction/c_instruction.c
@@ -124,13 +124,14 @@ void split_C_instruction(char* c_instruction, char* dest, char* comp, char* jump
             i = 0;
         }
 
+        z = 0;
         j = terminatorPos;
         while (i < j) {
             comp[z] = c_instruction[i];
             i++;
             z++;
         }
-        comp[i] = '\0';
+        comp[z] = '\0';
     }
 }
 
