dfa6ce6899241c7ace8ff286dc2951cc30607b12
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-11-03 09:00:05 +0100

    fix (c_instruction.c), split_C_instruction(), fix z variable not being properly used, got rid of printf debug statement, add null terminator to null char array

--- C-HackAssembler/C-instruction/c_instruction.c
+++ C-HackAssembler/C-instruction/c_instruction.c
@@ -83,13 +83,12 @@ void split_C_instruction(char* c_instruction, char* dest, char* comp, char* jump
         j = equalsPos;
         while (i < j) {
             dest[i] = c_instruction[i];
-            printf("I'm in equalspos");
             i++;
         }
         dest[i] = '\0';
         i++;
     } else {
-        dest[0] = 'n'; dest[1] = 'u'; dest[2] = 'l'; dest[3] = 'l';
+        dest[0] = 'n'; dest[1] = 'u'; dest[2] = 'l'; dest[3] = 'l'; dest[4] = '\0';
     }
     if (semiColonPos != 0) { // if jump field exists
         if (equalsPos != 0) {
@@ -116,7 +115,7 @@ void split_C_instruction(char* c_instruction, char* dest, char* comp, char* jump
         }
         jump[z] = '\0';
     } else {
-        jump[0] = 'n'; jump[1] = 'u'; jump[2] = 'l'; jump[3] = 'l';
+        jump[0] = 'n'; jump[1] = 'u'; jump[2] = 'l'; jump[3] = 'l'; jump[3] = '\0';
     }
     if (semiColonPos == 0) { // if no jump field exists
         if (equalsPos != 0) {
@@ -125,13 +124,14 @@ void split_C_instruction(char* c_instruction, char* dest, char* comp, char* jump
             i = 0;
         }
 
+        z = 0;
         j = terminatorPos;
         while (i < j) {
             comp[z] = c_instruction[i];
             i++;
             z++;
         }
-        comp[i] = '\0';
+        comp[z] = '\0';
     }
 }
 
